7a4a5569eada45a770694204f0df96569372f95c
heeyun <ato8757@gmail.com>
2021-12-31 18:00:07 +0900

    [추가] 헤더 최상단 로그인, 회원가입, 언어 선택 드롭 다운 구현

--- src/components/HeaderNav/HeaderNav.js
+++ src/components/HeaderNav/HeaderNav.js
@@ -1,1 +1,60 @@
-// HeaderNav
+import React, { useState } from "react";
+import "./HeaderNav.scss";
+
+export default function HeaderNav() {
+  return (
+    <header className="HeaderNav">
+      <div className="headerTop">
+        <ul>
+          <li>Login</li>
+          <li>Join</li>
+          <li className="globalStore">
+            <a href="/">KOR</a>
+            <ul className="globalStoreList">
+              <li>
+                <a href="/en">ENG</a>
+              </li>
+              <li>
+                <a href="/jp">JPN</a>
+              </li>
+              <li>
+                <a href="/cn">CHN</a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+      <div className="headerBottom">
+        <div className="headerBottomInner">
+          <div className="headerMenu">
+            <ul>
+              <li className="petsMenu">
+                <a href="/list">PET</a>
+                {/* <ul className="petsList">
+                  <li>DOG</li>
+                  <li>CAT</li>
+                  <li>TURTLE</li>
+                  <li>HAMSTER</li>
+                  <li>BIRD</li>
+                </ul> */}
+              </li>
+              <li>PRODUCT</li>
+              <li>PICK</li>
+              <li>EVENT</li>
+              <li>BRAND</li>
+            </ul>
+          </div>
+          <div className="headerLogo">로고 넣는 자리</div>
+          <div className="userMenu">
+            <ul>
+              <li>사람 모양</li>
+              <li>돋보기</li>
+              <li>장바구니</li>
+              <li>즐겨찾기</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./Main.scss";
+import HeaderNav from "../../components/HeaderNav/HeaderNav";
 
 const Main = () => {
-  return <div className="main">INDEX</div>;
+  return (
+    <div className="main">
+      <HeaderNav />
+    </div>
+  );
 };
 
 export default Main;
