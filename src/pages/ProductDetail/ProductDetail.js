502f39a704c221a8a6bd354a315ca8f0869f57cf
LJHyeokk <clzlsqkq56@naver.com>
2022-01-04 22:28:42 +0900

    [추가] - 슬라이더

--- src/pages/ProductDetail/ProductDetail.js
+++ src/pages/ProductDetail/ProductDetail.js
@@ -1,8 +1,63 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Slider } from "./compo/slider";
+
 import "./ProductDetail.scss";
 
 const ProductDetail = () => {
-  return <div className="productDetail">DETAIL</div>;
+  useEffect(() => {
+    fetch(`http://localhost:8000/product/9`, {
+      methrod: "GET",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then(res => res.json())
+      .then(res => {
+        setOriginalSize(res.length);
+        setData(res[0]);
+        setImgList(
+          res.map(e => {
+            return e.url;
+          })
+        );
+        setOriginalImg(
+          res.map(e => {
+            return e.url;
+          })
+        );
+      });
+  }, []);
+
+  const [data, setData] = useState([]);
+  const [imgList, setImgList] = useState([]);
+  const [originalImg, setOriginalImg] = useState([]);
+  const [originalSize, setOriginalSize] = useState(0);
+
+  return (
+    <div className="ProductDetail">
+      <div className="detailArea">
+        <div className="mainArea">
+          {data && data.is_new ? <NewTag /> : null}
+          <Slider
+            imgList={imgList}
+            setImgList={setImgList}
+            originalImg={originalImg}
+            originalSize={originalSize}
+          />
+        </div>
+        <div className="sidebarArea" />
+      </div>
+    </div>
+  );
+};
+
+const NewTag = () => {
+  return (
+    <div className="newBox">
+      <img src="/images/new.png" alt="newtag" />
+    </div>
+  );
 };
 
 export default ProductDetail;
