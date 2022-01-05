2e8623db3f95fdedd1e0dc8ef7ad45b8b496b922
TaeJoonPark <tjpark1028@gmail.com>
2022-01-06 04:05:00 +0900

    [추가] 1.Footer 레이아웃 완성 (디테일한 동작들은 미완성)

--- src/pages/ProductList/ProductList.js
+++ src/pages/ProductList/ProductList.js
@@ -1,6 +1,7 @@
 import { React, useState, useEffect } from "react";
 import { Link, useParams, useLocation } from "react-router-dom";
 import HeaderNav from "../../components/HeaderNav/HeaderNav";
+import FooterNav from "../../components/Footer/Footer";
 import ProductCard from "../../components/ProductCard";
 import "./ProductList.scss";
 
@@ -115,6 +116,7 @@ function ProductList() {
           </section>
         </section>
       </section>
+      <FooterNav />
     </section>
   );
 }
