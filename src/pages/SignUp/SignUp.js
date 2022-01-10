6b1206b6ea7d862aa9669cb38b8f5d4d1dfa585e
ebchoi <eunbichoi1@gmail.com>
2022-01-10 16:57:17 +0900

    [수정] - 오타 수정

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,4 +1,4 @@
-im1port React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import HeaderNav from "../../components/HeaderNav/HeaderNav";
 import Footer from "../../components/Footer/Footer";
