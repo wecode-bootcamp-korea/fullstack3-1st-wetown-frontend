c918cf05b1cd711b805eeb780004f33227ad14f1
ebchoi <eunbichoi1@gmail.com>
2022-01-11 14:05:57 +0900

    [추가] 메인슬라이드 이미지 옆에 문구 추가

--- src/components/Sliders/MainSlider/MainSlider.js
+++ src/components/Sliders/MainSlider/MainSlider.js
@@ -111,6 +111,7 @@ export default function MainSlider() {
                   key={obj.id}
                   alt="슬라이더 이미지"
                 />
+                <div className="mainSliderDetail">{obj.desc}</div>
               </li>
             );
           })}
